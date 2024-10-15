5299b8ef9f91c53c22bab3f6c51072dcd2a4db2c
jaypbazar <jabazar@my.cspc.edu.ph>
2024-10-15 08:43:04 +0800

    5

--- LT1.asm
+++ LT1.asm
@@ -1,35 +1,47 @@
 section .data
-    title1 db "==== SIMPLE CALCULATOR by Bazar & Garcia ====", 10
-    title2 db "==== ADDITION ====", 10
-    title3 db "==== SUBTRACTION ====", 10
-    title4 db "==== MULTIPLICATION ====", 10
-    title5 db "==== DIVISION ====", 10
+    ; variables for displaying the title messages
+    title1 db "==== SIMPLE CALCULATOR by Bazar & Garcia ====", 10, 0
+    title2 db "==== ADDITION ====", 10, 0
+    title3 db "==== SUBTRACTION ====", 10, 0
+    title4 db "==== MULTIPLICATION ====", 10, 0
+    title5 db "==== DIVISION ====", 10, 0
+    
+    ; variables for displaying the menu messages
+    menu1 db "[0] Exit", 10, 0
+    menu2 db "[1] Add", 10, 0
+    menu3 db "[2] Subtract", 10, 0
+    menu4 db "[3] Multiply", 10, 0
+    menu5 db "[4] Divide", 10, 0
 
-    menu1 db "[0] Exit", 10
-    menu2 db "[1] Add", 10
-    menu3 db "[2] Subtract", 10
-    menu4 db "[3] Multiply", 10
-    menu5 db "[4] Divide", 10
+    ; variables for displaying the output messages
+    sum db "Sum: %d", 10, 0
+    diff db "Difference: ", 10, 0
+    prod db "Product: %d", 10, 0
+    quot db "Quotient: %d", 10, 0
 
-    sum db "Sum: %d", 10
-    diff db "Difference: ", 10
-    prod db "Product: %d", 10
-    quot db "Quotient: %d", 10
+    ; variables for displaying the prompts
+    prompt1 db "Enter choice: ", 10, 0
+    prompt2 db "Enter the first number: ", 10, 0
+    prompt3 db "Enter the second number: ", 10, 0
+    prompt4 db "Thank you!", 10, 0
 
-    prompt1 db "Enter choice: ", 10
-    prompt2 db "Enter the first number: ", 10
-    prompt3 db "Enter the second number: ", 10
-    prompt4 db "Thank you!", 10
+    ; format of the inputs
+    inputFormat db "%d", 0
 
+    ; variables for displaying the error messages
     errorMessage1 db "Input should only be between -99 to 99. Please enter again a valid input.", 10
     errorMessage2 db "You cannot divide by 0. Please enter again a valid divisor.", 10
 
 section .bss
 
 section .text
+    ; external functions for input and output
     extern _printf
     extern _scanf
-    global _main
+
+    ; defining main funtion
+    global _main 
+
 
 _main:
 
